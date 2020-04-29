27f0cfd84ea64e4ebea5215e9039b1737dcfd3dd
gjack <gabi@ministrycentered.com>
2020-04-29 15:35:32 -0400

    Run migrations. Configure registration page for users

--- db/migrate/20200429185456_create_users.rb
+++ db/migrate/20200429185456_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      t.belong_to :organization
+      t.belongs_to :organization
       t.string :first_name
       t.string :last_name
 
