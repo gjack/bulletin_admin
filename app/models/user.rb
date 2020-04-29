cfb95c4d4e1e79c9ef01291f60c4987e107c6043
gjack <gabi@ministrycentered.com>
2020-04-29 15:09:08 -0400

    Initialize devise for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  belongs_to :organization
+  accepts_nested_attributes_for :organization
 end
